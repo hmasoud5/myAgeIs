cb55cae69bb6bf6e6b90bb91a062e038de977dfe
Heba Masoud <heba.masoud@patelhs.org>
2020-09-23 18:10:14 +0000

    Started if statement

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Hello world!");//
+    int myAge = 15;
+    int myBirthday = 416;
+    int todaysDate = 923;
+
+    if (myBirthday == todaysDate)
+     {
+       myAge = myAge + 1;
+       System.out.println("");
+     }
+     else
+     {
+
+     
+       System.out.println();
+     }
   }
 }
