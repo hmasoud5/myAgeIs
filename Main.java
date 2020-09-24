650910f14eca6926029b8a21c57cb7af34d29281
Heba Masoud <heba.masoud@patelhs.org>
2020-09-24 01:41:38 +0000

    string

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Hello world!");//
+    int myAge = 15;
+    int myBirthday = 416;
+    int todaysDate = 923;
+
+    if (myBirthday == todaysDate)
+     {
+       myAge = myAge + 1;
+       System.out.println("I am + myAge + yearsOld");
+     }
+     else
+     {
+
+       System.out.println();
+     }
   }
 }
