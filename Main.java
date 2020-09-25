54bfc00684dc99251ec48d7cd96b3c3f6a657ea7
Heba Masoud <heba.masoud@patelhs.org>
2020-09-25 16:13:30 +0000

    Added if/else statement

--- Main.java
+++ Main.java
@@ -1,19 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");//
+   
     int myAge = 15;
     int myBirthday = 416;
     int todaysDate = 923;
 
-    if (myBirthday == todaysDate)
-     {
-       myAge = myAge + 1;
-       System.out.println("I am + myAge + yearsOld");
-     }
-     else
-     {
+    if (myBirthday == todaysDate){
+      myAge = myAge + 1;
 
-       System.out.println("");
+       System.out.println("I am "+ myAge + " yearsOld");
+    }else {
+       System.out.println(" I am still " + myAge );
      }
   }
 }
